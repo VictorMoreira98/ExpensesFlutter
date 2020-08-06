ff5fb59dffeff6f07fea80248ca07d6f3a9afeac
VictorMoreira98 <39058177+VictorMoreira98@users.noreply.github.com>
2020-08-06 10:25:15 -0300

    Add files via upload

---
+++ lib/components/adaptative_button.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class AdaptativeButton extends StatelessWidget {
+  @override
+
+  final String label;
+  final Function onPressed;
+
+  AdaptativeButton({
+    this.label,
+    this.onPressed,
+  });
+
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+      ? CupertinoButton(
+        child: Text(label),
+        onPressed: onPressed,
+        color: Theme.of(context).primaryColor,
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+      )
+
+      : RaisedButton(
+        color: Theme.of(context).primaryColor,
+        textColor: Theme.of(context).textTheme.button.color,
+        child: Text(label),
+        onPressed: onPressed,
+      );
+  }
+}
